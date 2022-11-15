d9226ca56c2db79e052dbafeb1058043aa51404b
Lanix2 <david.r22767@gmail.com>
2022-11-15 19:52:08 -0300

    adiciones

--- server.js
+++ server.js
@@ -3,13 +3,14 @@ const {default: helmet} = require('helmet')
 const cors = require('cors')
 const morgan = require('morgan')
 const app = express()
+const fetch = require('node-fetch');
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-
-
+// "node-fetch": "2.0",
+//https://api-remaf.onrender.com/api
 //middlewares
 app.use(helmet())
 app.use(cors())
@@ -26,3 +27,14 @@ const port = process.env.PORT || 3000
 app.listen(port, () => {
     console.log(`Corriendo en el puerto ${port}`)
   })
+
+  
+const remaf = async () => {
+  const response = await fetch('https://api-remaf.onrender.com/api');
+  const body = await response.json()
+  console.log(body)
+  return body
+}
+
+
+remaf()
